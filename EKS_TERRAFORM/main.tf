c287521d8c7d35cad13a251dd4f05af2cfe73b9b
shekharb23 <shekharbarure0852@gmail.com>
2026-07-24 20:39:18 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -31,6 +31,17 @@ data "aws_subnets" "public" {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+
+  filter {
+    name   = "availability-zone"
+    values = [
+      "us-east-1a",
+      "us-east-1b",
+      "us-east-1c",
+      "us-east-1d",
+      "us-east-1f"
+    ]
+  }
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
